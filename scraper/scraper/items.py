5fa261db959b6daaac514bcce5ce32580658b63b
Mark O'Reilly <marko@megsupporttools.com>
2021-01-21 09:10:56 +0000

    added items

--- scraper/scraper/items.py
+++ scraper/scraper/items.py
@@ -1,12 +1,12 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
+from scrapy_djangoitem import DjangoItem
+
+from cms.models import Product, ProductAttribute
 
 
-class ScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class ProductItem(DjangoItem):
+    django_model = Product
+
+
+class ProductAttributeItem(DjangoItem):
+    django_model = ProductAttribute
