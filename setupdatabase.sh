ac5a59ace457a62a28fd3378cf9516634f2b6ac8
Mark O'Reilly <marko@megsupporttools.com>
2021-01-26 08:38:13 +0000

    replaced file with linux line endings

---
+++ setupdatabase.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# Don't create database if for example using a docker image that has created an empty database
+
+./manage.py migrate --run-syncdb
+./manage.py createsuperuser --username=admin --email=me@mor.dev --noinput
+./manage.py set_fake_passwords
+./manage.py runscript --traceback load_cms
+./manage.py set_fake_passwords
+exit 0
