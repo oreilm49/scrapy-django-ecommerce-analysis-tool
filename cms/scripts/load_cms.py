866ef0669efbd7c06a66a29e5ff640eac493a0c3
Mark O'Reilly <marko@megsupporttools.com>
2021-01-27 20:57:10 +0000

    updated load_cms

--- cms/scripts/load_cms.py
+++ cms/scripts/load_cms.py
@@ -1,7 +1,7 @@
 from django.db import transaction
 from model_mommy import mommy
 
-from cms.constants import CATEGORY, TABLE_LABEL_COLUMN, TABLE_VALUE_COLUMN, TABLE, STRING, FLOAT, TEXT
+from cms.constants import CATEGORY, TABLE_LABEL_COLUMN, TABLE_VALUE_COLUMN, TABLE, STRING, FLOAT, TEXT, LINK, PAGINATION
 from cms.models import Website, Category, Url, Selector, PageDataItem
 
 
@@ -11,16 +11,18 @@ def set_up_websites():
     washing_machines, _ = Category.objects.get_or_create(name="washing machines", parent=laundry, alternate_names=["washers", "front loaders"])
     Url.objects.get_or_create(url="https://www.harveynorman.ie/home-appliances/appliances/washing-machines/", url_type=CATEGORY, website=harvey_norman, category=washing_machines)
 
-    table_selector: Selector = mommy.make(Selector, selector_type=TABLE, css_selector="#content_features table.table-product-features tr", website=harvey_norman)
-    mommy.make(Selector, selector_type=TABLE_LABEL_COLUMN, css_selector="th", website=harvey_norman, parent=table_selector)
-    mommy.make(Selector, selector_type=TABLE_VALUE_COLUMN, css_selector="td", website=harvey_norman, parent=table_selector)
-    mommy.make(PageDataItem, name="table", data_type=STRING, website=harvey_norman, selector=table_selector)
-    mommy.make(PageDataItem, name="price", data_type=FLOAT, website=harvey_norman, selector=mommy.make(
-        Selector, selector_type=TEXT, css_selector=".price-num", website=harvey_norman
+    table_selector, _ = Selector.objects.get_or_create(selector_type=TABLE, css_selector="#content_features table.table-product-features tr", website=harvey_norman)
+    Selector.objects.get_or_create(selector_type=TABLE_LABEL_COLUMN, css_selector="th strong::text", website=harvey_norman, parent=table_selector)
+    Selector.objects.get_or_create(selector_type=TABLE_VALUE_COLUMN, css_selector="td::text", website=harvey_norman, parent=table_selector)
+    PageDataItem.objects.get_or_create(name="table", data_type=STRING, website=harvey_norman, selector=table_selector)
+    PageDataItem.objects.get_or_create(name="price", data_type=FLOAT, website=harvey_norman, selector=mommy.make(
+        Selector, selector_type=TEXT, css_selector=".price-num:nth-child(2)::text", website=harvey_norman
     ))
-    mommy.make(PageDataItem, name="model", data_type=STRING, website=harvey_norman, selector=mommy.make(
-        Selector, selector_type=TEXT, css_selector=".product-id.meta", website=harvey_norman
+    PageDataItem.objects.get_or_create(name="model", data_type=STRING, website=harvey_norman, selector=mommy.make(
+        Selector, selector_type=TEXT, css_selector=".product-id.meta::text", website=harvey_norman
     ))
+    Selector.objects.get_or_create(selector_type=LINK, css_selector=".product-info a", website=harvey_norman)
+    Selector.objects.get_or_create(selector_type=PAGINATION, css_selector="li a.next", website=harvey_norman)
 
 
 @transaction.atomic()
