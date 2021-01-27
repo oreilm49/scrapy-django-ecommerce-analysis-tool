594327db189559c2789c49dce75ff5284c7ba22d
Mark O'Reilly <marko@megsupporttools.com>
2021-01-27 19:37:29 +0000

    added code to run spider from command line

--- scraper/scripts/crawler.py
+++ scraper/scripts/crawler.py
@@ -1,9 +1,18 @@
+from django.db import transaction
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
-from spiders.ecommerce import EcommerceSpider
+
+from cms.models import Website
+from scraper.spiders.ecommerce import EcommerceSpider
 
 
 def run_spider(website):
     process = CrawlerProcess(get_project_settings())
     process.crawl(EcommerceSpider, website=website)
     process.start()
+
+
+@transaction.atomic()
+def run():
+    for website in Website.objects.filter(publish=True):
+        run_spider(website)
