f6e98b94dc5f2ce5606f78527e64bfeb37df5aac
Mark O'Reilly <marko@megsupporttools.com>
2021-01-31 14:56:44 +0000

    added Value constant

--- cms/data_processing/constants.py
+++ cms/data_processing/constants.py
@@ -2,3 +2,4 @@ from collections import namedtuple
 
 ProcessedAttribute = namedtuple("ProcessedAttribute", ["attribute_type", "unit", "amount"])
 UnitValue = namedtuple("UnitValue", ["unit", "value"])
+Value = namedtuple("Value", ["value"])
