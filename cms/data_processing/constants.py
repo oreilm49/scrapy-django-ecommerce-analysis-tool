fdf1cfa320042f87d83b215fe3796d41da675129
Mark O'Reilly <marko@megsupporttools.com>
2021-02-01 08:34:15 +0000

    added RangeUnitValue constant

--- cms/data_processing/constants.py
+++ cms/data_processing/constants.py
@@ -3,3 +3,4 @@ from collections import namedtuple
 ProcessedAttribute = namedtuple("ProcessedAttribute", ["attribute_type", "unit", "amount"])
 UnitValue = namedtuple("UnitValue", ["unit", "value"])
 Value = namedtuple("Value", ["value"])
+RangeUnitValue = namedtuple("RangeUnitValue", ["unit", "value_low", "value_high"])
