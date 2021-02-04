f77444098beb4e7d5387c156873100adb7627aef
Mark O'Reilly <marko@megsupporttools.com>
2021-02-04 19:01:04 +0000

    added tests for price and image pipelines

--- scraper/tests/test_pipeline.py
+++ scraper/tests/test_pipeline.py
@@ -3,9 +3,12 @@ from typing import List, Dict
 from django.test import TestCase
 from model_mommy import mommy
 
-from cms.models import Category, Product, ProductAttribute, Unit
+from cms.constants import PRICE, MAIN, THUMBNAIL
+from cms.models import Category, Product, ProductAttribute, Unit, Website, Selector, WebsiteProductAttribute, \
+    AttributeType, ProductImage
 
-from scraper.pipelines import ProductPipeline, ProductAttributePipeline
+from scraper.pipelines import ProductPipeline, ProductAttributePipeline, WebsiteProductAttributePipeline, \
+    ProductImagePipeline
 from scraper.items import ProductPageItem
 
 
@@ -14,6 +17,7 @@ class TestPipeline(TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         super().setUpClass()
+        cls.website: Website = mommy.make(Website, name="test_website", currency__name="€")
         cls.category: Category = mommy.make(Category, name="washing machines")
         cls.product: Product = mommy.make(Product, model="model_number")
 
@@ -61,3 +65,26 @@ class TestPipeline(TestCase):
             item['attributes']: List[Dict] = [{'value': 'a+++', 'label': 'energy rating'}]
             ProductAttributePipeline().process_item(item, {})
             self.assertTrue(ProductAttribute.objects.filter(product=self.product, attribute_type__name='energy rating', value="a+++").exists())
+
+    def test_website_product_attribute(self):
+        item: ProductPageItem = ProductPageItem(product=self.product, category=self.category, website=self.website)
+        item['website_attributes']: List[Dict] = [{
+            'value': '399.99',
+            'selector': mommy.make(Selector, website=self.website, selector_type=PRICE)
+        }, {
+            'value': '499.99',
+            'selector': mommy.make(Selector, website=self.website, selector_type="invalid selector")
+        }]
+        WebsiteProductAttributePipeline().process_item(item, {})
+        attribute_type: AttributeType = AttributeType.objects.get(name="price", unit=self.website.currency)
+        self.assertTrue(WebsiteProductAttribute.objects.filter(website=self.website, attribute_type=attribute_type, product=self.product, value="399.99").exists())
+        self.assertFalse(WebsiteProductAttribute.objects.filter(website=self.website, attribute_type=attribute_type, product=self.product, value="499.99").exists())
+
+    def test_product_image_pipeline(self):
+        item: ProductPageItem = ProductPageItem(product=self.product, images=[{'path': 'full/testimage.jpg'}])
+        ProductImagePipeline().process_item(item, {})
+        self.assertTrue(ProductImage.objects.filter(product=self.product, image_type=MAIN, image='full/testimage.jpg').exists())
+        self.assertTrue(ProductImage.objects.filter(product=self.product, image_type=THUMBNAIL, image='thumbs/big/testimage.jpg').exists())
+        item['images'] = [{'path': 'full/testimage2.jpg'}]
+        self.assertFalse(ProductImage.objects.filter(product=self.product, image_type=MAIN, image='full/testimage2.jpg').exists())
+        self.assertFalse(ProductImage.objects.filter(product=self.product, image_type=THUMBNAIL, image='thumbs/big/testimage2.jpg').exists())
