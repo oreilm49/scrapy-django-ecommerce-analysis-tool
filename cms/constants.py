7ccfbf200ef85e14417f7803a541a74964bca9cd
Mark O'Reilly <marko@megsupporttools.com>
2021-02-14 13:51:55 +0000

    added widget types and choices

--- cms/constants.py
+++ cms/constants.py
@@ -1,4 +1,11 @@
+from collections import OrderedDict
+from typing import Dict, Type
+
+from django import forms
 from django.utils.translation import gettext as _
+
+from cms.form_widgets import FloatInput
+from cms.utils import get_dotted_path
 
 MAX_LENGTH = 100
 
@@ -65,3 +72,25 @@ IMAGE_TYPES = (
     (MAIN, _("Main")),
     (THUMBNAIL, _("Thumbnail")),
 )
+
+# Dictionary of supported widgets mapped to form field class
+WIDGETS: Dict[Type[forms.Widget], Type[forms.Field]] = OrderedDict([
+    (forms.widgets.TextInput, forms.CharField),
+    (forms.widgets.NumberInput, forms.IntegerField),
+    (FloatInput, forms.FloatField),
+    (forms.widgets.CheckboxInput, forms.BooleanField),
+    (forms.widgets.DateTimeInput, forms.DateTimeField),
+])
+
+# Set of supported field type classes
+FIELD_TYPES = set(WIDGETS.values())
+
+WIDGET_NAMES = {
+    forms.widgets.TextInput: _("Text"),
+    forms.widgets.NumberInput: _("Number (integer)"),
+    forms.widgets.CheckboxInput: _("Checkbox"),
+    forms.widgets.DateTimeInput: _("Date & Time"),
+    FloatInput: _("Number (decimal)"),
+}
+
+WIDGET_CHOICES = [(get_dotted_path(cls), WIDGET_NAMES.get(cls, cls.__name__)) for cls in WIDGETS.keys()]
