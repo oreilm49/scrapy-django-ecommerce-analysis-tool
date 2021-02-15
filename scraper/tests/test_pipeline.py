4e8faf8fc8b109e3e532f81e77a0ac3e70bab228
Mark O'Reilly <marko@megsupporttools.com>
2021-02-15 20:06:56 +0000

    fixed tests

--- scraper/tests/test_pipeline.py
+++ scraper/tests/test_pipeline.py
@@ -4,8 +4,10 @@ from django.test import TestCase
 from model_mommy import mommy
 
 from cms.constants import PRICE, MAIN, THUMBNAIL
+from cms.form_widgets import FloatInput
 from cms.models import Category, Product, ProductAttribute, Unit, Website, Selector, WebsiteProductAttribute, \
     AttributeType, ProductImage
+from cms.utils import get_dotted_path
 
 from scraper.pipelines import ProductPipeline, ProductAttributePipeline, WebsiteProductAttributePipeline, \
     ProductImagePipeline
@@ -17,7 +19,7 @@ class TestPipeline(TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         super().setUpClass()
-        cls.website: Website = mommy.make(Website, name="test_website", currency__name="€")
+        cls.website: Website = mommy.make(Website, name="test_website", currency__name="€", currency__widget=get_dotted_path(FloatInput))
         cls.category: Category = mommy.make(Category, name="washing machines")
         cls.product: Product = mommy.make(Product, model="model_number", category=cls.category)
 
@@ -49,7 +51,7 @@ class TestPipeline(TestCase):
             self.assertFalse(Unit.objects.filter(name="kilogram").first())
             item['attributes']: List[Dict] = [{'value': '10kg', 'label': 'Wash capacity'}]
             ProductAttributePipeline().process_item(item, {})
-            self.assertTrue(ProductAttribute.objects.filter(product=self.product, attribute_type__name='Wash capacity', value="10").exists())
+            self.assertTrue(ProductAttribute.objects.filter(product=self.product, attribute_type__name='Wash capacity', data__value=10).exists())
             self.assertTrue(Unit.objects.filter(name="kilogram").first())
         with self.subTest("range unit"):
             self.assertFalse(ProductAttribute.objects.filter(product=self.product, attribute_type__name='power - low').exists())
@@ -57,14 +59,14 @@ class TestPipeline(TestCase):
             self.assertFalse(Unit.objects.filter(name="volt").first())
             item['attributes']: List[Dict] = [{'value': '220 - 240v', 'label': 'power'}]
             ProductAttributePipeline().process_item(item, {})
-            self.assertTrue(ProductAttribute.objects.filter(product=self.product, attribute_type__name='power - low', value="220").exists())
-            self.assertTrue(ProductAttribute.objects.filter(product=self.product, attribute_type__name='power - high', value="240").exists())
+            self.assertTrue(ProductAttribute.objects.filter(product=self.product, attribute_type__name='power - low', data__value=220).exists())
+            self.assertTrue(ProductAttribute.objects.filter(product=self.product, attribute_type__name='power - high', data__value=240).exists())
             self.assertTrue(Unit.objects.filter(name="volt").first())
         with self.subTest("energy rating"):
             self.assertFalse(ProductAttribute.objects.filter(product=self.product, attribute_type__name='energy rating').exists())
             item['attributes']: List[Dict] = [{'value': 'a+++', 'label': 'energy rating'}]
             ProductAttributePipeline().process_item(item, {})
-            self.assertTrue(ProductAttribute.objects.filter(product=self.product, attribute_type__name='energy rating', value="a+++").exists())
+            self.assertTrue(ProductAttribute.objects.filter(product=self.product, attribute_type__name='energy rating', data__value="a+++").exists())
 
     def test_website_product_attribute(self):
         item: ProductPageItem = ProductPageItem(product=self.product, category=self.category, website=self.website)
@@ -77,8 +79,8 @@ class TestPipeline(TestCase):
         }]
         WebsiteProductAttributePipeline().process_item(item, {})
         attribute_type: AttributeType = AttributeType.objects.get(name="price", unit=self.website.currency)
-        self.assertTrue(WebsiteProductAttribute.objects.filter(website=self.website, attribute_type=attribute_type, product=self.product, value="399.99").exists())
-        self.assertFalse(WebsiteProductAttribute.objects.filter(website=self.website, attribute_type=attribute_type, product=self.product, value="499.99").exists())
+        self.assertTrue(WebsiteProductAttribute.objects.filter(website=self.website, attribute_type=attribute_type, product=self.product, data__value=399.99).exists())
+        self.assertFalse(WebsiteProductAttribute.objects.filter(website=self.website, attribute_type=attribute_type, product=self.product, data__value=499.99).exists())
 
     def test_product_image_pipeline(self):
         item: ProductPageItem = ProductPageItem(product=self.product, images=[{'path': 'full/testimage.jpg'}])
