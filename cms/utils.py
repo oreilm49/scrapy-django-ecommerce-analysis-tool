3df18617d9f83adb99758de78369d99e63ff5237
Mark O'Reilly <marko@megsupporttools.com>
2021-02-18 08:05:05 +0000

    added reusable methods for form

--- cms/utils.py
+++ cms/utils.py
@@ -1,3 +1,4 @@
+import datetime
 from typing import List, Union, Optional
 
 from django.db.models import QuerySet
@@ -39,3 +40,15 @@ def products_grouper(product: 'Product', attribute: 'AttributeType', attribute_v
 def get_dotted_path(cls: type) -> str:
     """Returns python dotted path for class"""
     return u'{}.{}'.format(cls.__module__, cls.__name__)
+
+
+def serialized_values_for_attribute_type(values: List[Union[str, int, float, datetime.datetime]], attribute_type: 'AttributeType') -> List[Union[str, int, float, datetime.datetime]]:
+    if attribute_type.unit:
+        return [attribute_type.unit.serializer.serializer(value) for value in values]
+    return values
+
+
+def is_value_numeric(value: Union[int, str, float]):
+    if isinstance(value, datetime.datetime):
+        return False
+    return type(value) in [float, int] or value.replace('.', '', 1).isdigit()
