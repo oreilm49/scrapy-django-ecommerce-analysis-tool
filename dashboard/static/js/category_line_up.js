4882f8b0dc223519329d672f139216029e38f7fa
Mark O'Reilly <marko@megsupporttools.com>
2021-02-18 09:01:09 +0000

    added modal for category table form

---
+++ dashboard/static/js/category_line_up.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+   var category_line_up_form = new FormModal('category_line_up_form');
+   $('document').on('click', '#category_line_up_show', function (){
+      category_line_up_form.show()
+   });
+   $('document').on('click', '#category_line_up_submit', function (){
+      $('#category_line_up_form').submit();
+   });
+});
