989039b208110d74649d979440616afd0cf467cd
Mark O'Reilly <marko@megsupporttools.com>
2021-02-28 13:29:05 +0000

    added create view

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,22 +2,44 @@ import itertools
 from collections import namedtuple
 from typing import Iterator, Tuple
 
-from django.views.generic import ListView, DetailView
+from django.urls import reverse
+from django.views.generic import ListView, DetailView, CreateView
 
 from dashboard.forms import CategoryTableForm
 from cms.models import Product
 from cms.utils import products_grouper
 from dashboard.models import CategoryTable
+from dashboard.toolbar import ToolbarItem
 
 CategoryTableProduct = namedtuple('CategoryTableProduct', ['x_axis_grouper', 'y_axis_grouper', 'product'])
 
 
-class CategoryTables(ListView):
+class CategoryTableMixin:
+
+    def get_context_data(self, **kwargs):
+        data: dict = super().get_context_data(**kwargs)
+        data.update(
+            toolbar_items=[ToolbarItem(
+                label='Create',
+                url=reverse('category-table-create'),
+                icon='fa fa-plus'
+            )]
+        )
+        return data
+
+
+class CategoryTables(CategoryTableMixin, ListView):
     template_name = 'views/category_tables.html'
     queryset = CategoryTable.objects.published()
 
 
-class CategoryTableDetail(DetailView):
+class CategoryTableCreate(CategoryTableMixin, CreateView):
+    template_name = 'views/category_table_modify.html'
+    queryset = CategoryTable.objects.published()
+    form_class = CategoryTableForm
+
+
+class CategoryTableDetail(CategoryTableMixin, DetailView):
     template_name = 'views/category_table.html'
 
     def get_form(self):
