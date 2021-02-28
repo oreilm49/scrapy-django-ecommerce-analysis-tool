14bae0272480fd477dc431229c48c81c63b4c336
Mark O'Reilly <marko@megsupporttools.com>
2021-02-28 13:29:17 +0000

    added urls for create detail and list view

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from dashboard.views import CategoryTables, CategoryTableDetail
+from dashboard.views import CategoryTables, CategoryTableDetail, CategoryTableCreate
 
 urlpatterns = [
-    url(r'category-tables/', CategoryTables.as_view(), name='category-tables'),
-    url(r'category-table/(?P<pk>\d+)/', CategoryTableDetail.as_view(), name='category-table'),
+    url(r'category-tables/$', CategoryTables.as_view(), name='category-tables'),
+    url(r'category-tables/add/$', CategoryTableCreate.as_view(), name='category-table-create'),
+    url(r'category-table/(?P<pk>\d+)/$', CategoryTableDetail.as_view(), name='category-table'),
 ]
