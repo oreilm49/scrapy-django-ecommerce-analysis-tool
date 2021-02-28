2c99790018716f61a9da49d420ae04f0a3b5fef6
Mark O'Reilly <marko@megsupporttools.com>
2021-02-28 12:20:28 +0000

    removed unused urls

--- cms/urls.py
+++ cms/urls.py
@@ -8,6 +8,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('dashboard/', include('dashboard.urls'))
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
