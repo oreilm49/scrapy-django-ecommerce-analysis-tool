e26aed5621233d30fe54d24805491549ff179ea6
Mark O'Reilly <marko@megsupporttools.com>
2021-02-28 17:29:05 +0000

    added update url

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from dashboard.views import CategoryTables, CategoryTableDetail, CategoryTableCreate
+from dashboard.views import CategoryTables, CategoryTableDetail, CategoryTableCreate, CategoryTableUpdate
 
 urlpatterns = [
     url(r'category-tables/$', CategoryTables.as_view(), name='category-tables'),
     url(r'category-tables/add/$', CategoryTableCreate.as_view(), name='category-table-create'),
     url(r'category-table/(?P<pk>\d+)/$', CategoryTableDetail.as_view(), name='category-table'),
+    url(r'category-table/(?P<pk>\d+)/update$', CategoryTableUpdate.as_view(), name='category-table-update'),
 ]
