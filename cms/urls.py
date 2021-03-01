7b41647ce45e0b114f7d16c52d9bd389d144ce94
Mark O'Reilly <marko@megsupporttools.com>
2021-03-01 19:24:55 +0000

    added JavaScriptCatalog url

--- cms/urls.py
+++ cms/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.i18n import JavaScriptCatalog
 
-from cms import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dashboard/', include('dashboard.urls'))
+    path('dashboard/', include('dashboard.urls')),
+    path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
 ]
