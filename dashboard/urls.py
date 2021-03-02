589c6c9262dd724275779f5256a9a7fb93548729
Mark O'Reilly <marko@megsupporttools.com>
2021-03-02 20:04:37 +0000

    added dashboard home view

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 
-from dashboard.views import CategoryTables, CategoryTableDetail, CategoryTableCreate, CategoryTableUpdate
+from dashboard.views import CategoryTables, CategoryTableDetail, CategoryTableCreate, CategoryTableUpdate, DashboardHome
 
 urlpatterns = [
-    url('', CategoryTables.as_view(), name='home'),
+    url(r'home/$', DashboardHome.as_view(), name='home'),
     url(r'category-tables/$', CategoryTables.as_view(), name='category-tables'),
     url(r'category-tables/add/$', CategoryTableCreate.as_view(), name='category-table-create'),
     url(r'category-table/(?P<pk>\d+)/$', CategoryTableDetail.as_view(), name='category-table'),
