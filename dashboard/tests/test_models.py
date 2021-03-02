db02a6af8a0e81b4f1f2102cf7ece2ca5bf41175
Mark O'Reilly <marko@megsupporttools.com>
2021-03-02 19:13:24 +0000

    added test for models

---
+++ dashboard/tests/test_models.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from model_mommy import mommy
+
+from cms.models import Product, ProductAttribute, WebsiteProductAttribute, AttributeType, Category
+from dashboard.models import CategoryTable
+
+
+class TestModels(TestCase):
+
+    def test_category_table_products(self):
+        category: Category = mommy.make(Category)
+        product_1: Product = mommy.make(Product, category=category, model="product_1")
+        product_2: Product = mommy.make(Product, category=category, model="product_2")
+        product_3: Product = mommy.make(Product, category=category, model="product_3")
+        product_4: Product = mommy.make(Product, category__name="dryers", model="product_4")
+        product_5: Product = mommy.make(Product, category=category, model="filtered_by_search")
+        brand_attr: AttributeType = mommy.make(AttributeType, name="brand")
+        price_attr: WebsiteProductAttribute = mommy.make(WebsiteProductAttribute, attribute_type__name="price")
+        mommy.make(ProductAttribute, attribute_type=brand_attr, product=product_1, data={"value": "whirlpool"})
+        mommy.make(ProductAttribute, attribute_type=brand_attr, product=product_2, data={"value": "hotpoint"})
+        mommy.make(ProductAttribute, attribute_type=brand_attr, product=product_3, data={"value": "indesit"})
+        mommy.make(ProductAttribute, attribute_type=brand_attr, product=product_4, data={"value": "hotpoint"})
+        mommy.make(ProductAttribute, attribute_type=brand_attr, product=product_5, data={"value": "hotpoint"})
+        table: CategoryTable = mommy.make(
+            CategoryTable,
+            x_axis_values=["0", "199", "299", "399", "499"],
+            y_axis_values=["whirlpool", "hotpoint"],
+            x_axis_attribute=price_attr.attribute_type,
+            y_axis_attribute=brand_attr,
+            category=category,
+            query="product_",
+            name="test",
+        )
+        products = table.products(Product.objects.published())
+        self.assertIn(product_1, products)
+        self.assertIn(product_2, products)
+        self.assertNotIn(product_3, products)
+        self.assertNotIn(product_4, products)
+        self.assertNotIn(product_5, products)
