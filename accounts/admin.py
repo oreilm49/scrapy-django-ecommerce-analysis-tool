5527e27a5dee700135d6bac0ad191754b2f77cf3
Mark O'Reilly <marko@megsupporttools.com>
2021-03-04 08:52:56 +0000

    added admin site for accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from accounts.models import Profile, Company, Team
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = 'id', 'user', 'company',
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name',
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name', 'company',
