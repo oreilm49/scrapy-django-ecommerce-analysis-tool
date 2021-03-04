6d3edd52de77fc9af2bd49028e86e52fcd465136
Mark O'Reilly <marko@megsupporttools.com>
2021-03-04 08:14:08 +0000

    added CategoryTableQuerySet with user field on CategoryTable

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,17 +1,28 @@
 from typing import List, TYPE_CHECKING
 
+from django.contrib.auth.models import User
 from django.db import models
-from django.db.models import QuerySet
+from django.db.models import QuerySet, Q
 from django.utils.translation import gettext as _
 
-from cms.models import BaseModel
+from cms.models import BaseModel, BaseQuerySet
 from cms.utils import is_value_numeric
 
 if TYPE_CHECKING:
     from cms.models import ProductQuerySet
 
 
+class CategoryTableQuerySet(BaseQuerySet):
+
+    def for_user(self, user: User):
+        """
+        Returns all category tables in the user's company.
+        """
+        return self.filter(Q(user=user) | Q(user__profile__company=user.profile.company))
+
+
 class CategoryTable(BaseModel):
+    user = models.ForeignKey(User, verbose_name=_("User"), on_delete=models.SET_NULL, null=True)
     name = models.CharField(verbose_name=_('Name'), max_length=100, help_text=_('The name for this dashboard'))
     x_axis_attribute = models.ForeignKey(
         "cms.AttributeType",
@@ -33,6 +44,8 @@ class CategoryTable(BaseModel):
     y_axis_values = models.JSONField(verbose_name=_('Y Axis Values'), help_text=_('The values products must have for the y axis attribute in order to appear in the table.'), default=list)
     category = models.ForeignKey("cms.Category", verbose_name=_('Category'), help_text=_('The category products should belong to in order to appear in the table.'), on_delete=models.SET_NULL, null=True)
     query = models.CharField(verbose_name=_('Search'), blank=True, null=True, max_length=100, help_text=_('General search text used to further filter products.'))
+
+    objects = CategoryTableQuerySet.as_manager()
 
     def products(self, queryset: 'ProductQuerySet') -> 'QuerySet':
         product_pks: List[int] = []
