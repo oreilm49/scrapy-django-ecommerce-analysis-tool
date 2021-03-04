e14eb6ee9cb93ae302c40977bde28ce87b95ed23
Mark O'Reilly <marko@megsupporttools.com>
2021-03-04 19:47:55 +0000

    added post save to make profile on user saved

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User, Permission
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils.translation import gettext as _
 
 from cms.constants import MAX_LENGTH
@@ -12,7 +14,15 @@ class Company(BaseModel):
 
 class Profile(BaseModel):
     user = models.OneToOneField(User, verbose_name=_("User"), on_delete=models.CASCADE)
-    company = models.ForeignKey(Company, verbose_name=_('Company'), on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, verbose_name=_('Company'), on_delete=models.CASCADE, null=True)
+
+
+@receiver(post_save, sender=User)
+def user_profile_saved(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    else:
+        instance.profile.save()
 
 
 class Team(BaseModel):
