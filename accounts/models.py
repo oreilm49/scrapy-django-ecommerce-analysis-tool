9ce1ebfa4356d674bed02f48d189aa5628e62ff2
Mark O'Reilly <marko@megsupporttools.com>
2021-03-04 07:58:01 +0000

    added accounts models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User, Permission
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+from cms.constants import MAX_LENGTH
+from cms.models import BaseModel
+
+
+class Company(BaseModel):
+    name = models.CharField(verbose_name=_("Name"), max_length=MAX_LENGTH, help_text=_("The company name"), unique=True)
+
+
+class Profile(BaseModel):
+    user = models.OneToOneField(User, verbose_name=_("Profile"), on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, verbose_name=_('Company'), on_delete=models.CASCADE)
+
+
+class Team(BaseModel):
+    name = models.CharField(verbose_name=_("Name"), max_length=MAX_LENGTH, help_text=_("The company name"), unique=True)
+    company = models.ForeignKey(Company, verbose_name=_('Company'), on_delete=models.CASCADE)
+    users = models.ManyToManyField(User, verbose_name=_("Users"))
+    permissions = models.ManyToManyField(Permission, verbose_name=_("Permissions"))
+
