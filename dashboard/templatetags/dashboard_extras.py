66f0a5f094b4c950d6af56511ad5bfed82b3f56e
Mark O'Reilly <marko@megsupporttools.com>
2021-03-05 08:39:49 +0000

    added product_specs template tag

---
+++ dashboard/templatetags/dashboard_extras.py
@@ -0,0 +1,18 @@
+from django import template
+from django.utils.html import format_html
+
+from cms.models import Product, ProductAttribute
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def product_specs(context: dict, product: Product):
+    specs_limit: int = 5
+    html = ""
+    for index, product_attribute in enumerate(product.top_attributes):
+        product_attribute: ProductAttribute
+        if index > specs_limit:
+            break
+        html += f"<div style='display: block'>{product_attribute.display}</div>"
+    return format_html(html)
