35704c04f6ccb7c26b967cb8f2dc082434d6f327
Mark O'Reilly <marko@megsupporttools.com>
2021-03-06 16:41:19 +0000

    added BaseDashboardMixin with navitems

--- dashboard/views/base.py
+++ dashboard/views/base.py
@@ -1,8 +1,11 @@
 from collections import namedtuple
 from typing import List, Optional
 
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 from django.views.generic import RedirectView
+
+from dashboard.toolbar import NavItem
 
 
 class DashboardHome(RedirectView):
@@ -26,5 +29,18 @@ class BreadcrumbMixin:
         data: dict = super().get_context_data(**kwargs)
         data.update(
             breadcrumbs=self.get_breadcrumbs()
+        )
+        return data
+
+
+class BaseDashboardMixin(LoginRequiredMixin, BreadcrumbMixin):
+
+    def get_context_data(self, **kwargs):
+        data: dict = super().get_context_data(**kwargs)
+        data.update(
+            nav_items=(
+                NavItem(label='Pivot', icon='fa fa-table', url=reverse('dashboard:category-tables'), active=reverse('dashboard:category-tables') in self.request.path),
+                NavItem(label='Products', icon='fa fa-shopping-cart', url=reverse('dashboard:products'), active=reverse('dashboard:products') in self.request.path),
+            )
         )
         return data
