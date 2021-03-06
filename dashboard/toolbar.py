b1707d1636fb977b890e8b9f5a12e9025fdcc439
Mark O'Reilly <marko@megsupporttools.com>
2021-03-06 16:41:07 +0000

    added NavItem class

--- dashboard/toolbar.py
+++ dashboard/toolbar.py
@@ -22,3 +22,27 @@ class ToolbarItem:
             icon=self.icon,
             help_text=self.help_text or '',
         )
+
+
+class NavItem:
+    def __init__(self, label, url, icon, active: Optional[bool] = False):
+        self.label = label
+        self.url = url
+        self.icon = icon
+        self.active = active
+
+    def render(self):
+        return format_html(
+            """
+            <li class="nav-item {active}">
+                <a class="nav-link" href="{url}">
+                    <i class="{icon}"></i>
+                    <span>{label}</span></a>
+            </li>
+            """,
+            label=self.label,
+            url=self.url,
+            icon=self.icon,
+            active='active' if self.active else '',
+        )
+
