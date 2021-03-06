9c3243d549e25c6cd208344b3679d52c72c758b5
Mark O'Reilly <marko@megsupporttools.com>
2021-03-06 13:18:30 +0000

    added a product view to show price history of product

--- dashboard/views/products.py
+++ dashboard/views/products.py
@@ -3,9 +3,9 @@ from typing import Optional, List
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
 
-from cms.models import Product, ProductQuerySet
+from cms.models import Product, ProductQuerySet, WebsiteProductAttributeQuerySet, WebsiteProductAttribute
 from dashboard.views.base import Breadcrumb, BreadcrumbMixin
 
 
@@ -20,13 +20,20 @@ class Products(LoginRequiredMixin, BreadcrumbMixin, ListView):
         ]
 
 
-class ProductDetail(LoginRequiredMixin, BreadcrumbMixin, DetailView):
+class ProductDetail(LoginRequiredMixin, BreadcrumbMixin, ListView):
+    paginate_by = 25
     template_name = 'views/product.html'
-    queryset: ProductQuerySet = Product.objects.published()
+    queryset: WebsiteProductAttributeQuerySet = WebsiteProductAttribute.objects.published()
 
-    def get_queryset(self) -> ProductQuerySet:
-        return self.queryset.for_user(self.request.user)
+    def get_queryset(self):
+        return super().get_queryset().filter(product=self.product, attribute_type__name="price")
 
     @property
     def product(self) -> Product:
-        return get_object_or_404(self.queryset, pk=self.request.resolver_match.kwargs.get('pk'))
+        return get_object_or_404(Product.objects.published(), pk=self.request.resolver_match.kwargs.get('pk'))
+
+    def get_breadcrumbs(self) -> Optional[List[Breadcrumb]]:
+        return [
+            Breadcrumb(name="Products", url=reverse('dashboard:products'), active=False),
+            Breadcrumb(name=self.product.model, url=reverse('dashboard:product', kwargs={'pk': self.product.pk}), active=True),
+        ]
