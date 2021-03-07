ce7fabc8981f7a963d0c8f32dcc75d585037b51e
Mark O'Reilly <marko@megsupporttools.com>
2021-03-07 17:40:59 +0000

    updated minio link

--- cms/settings.py
+++ cms/settings.py
@@ -145,7 +145,7 @@ STATIC_URL = '/static/'
 
 # File storage
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_S3_ENDPOINT_URL = 'http://minio:9000'
+AWS_S3_ENDPOINT_URL = os.environ.get('AWS_S3_ENDPOINT_URL', default='http://minio:9000')
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'product-images'
