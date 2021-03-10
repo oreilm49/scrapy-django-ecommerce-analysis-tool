681275c3b91656f1c9f1ac7d2259520c6286afd8
Mark O'Reilly <marko@megsupporttools.com>
2021-03-10 08:27:11 +0000

    avoid key errors for missing env variables

--- cms/settings.py
+++ cms/settings.py
@@ -86,10 +86,10 @@ WSGI_APPLICATION = 'cms.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['POSTGRES_DB'],
-        'USER': os.environ['POSTGRES_USER'],
-        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
-        'HOST': 'db',
+        'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
         'PORT': 5432,
         'TEST': {
             'NAME': os.environ.get('TEST_DB_NAME', default='test_cms'),
