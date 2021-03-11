63323bc82e404079bd1447d4bcf92cd7a8819ce4
Mark O'Reilly <marko@megsupporttools.com>
2021-03-11 08:48:06 +0000

    removed load minio script

--- setupdatabase.sh
+++ setupdatabase.sh
@@ -4,9 +4,8 @@ set -e
 # Don't create database if for example using a docker image that has created an empty database
 
 ./manage.py migrate --run-syncdb
-./manage.py createsuperuser --username=admin --email=me@mor.dev --noinput
+./manage.py createsuperuser --username=admin --email=markoreilly1992@gmail.com --noinput
 ./manage.py set_fake_passwords
 ./manage.py runscript --traceback load_accounts
 ./manage.py runscript --traceback load_cms
-./manage.py runscript --traceback load_minio
 exit 0
