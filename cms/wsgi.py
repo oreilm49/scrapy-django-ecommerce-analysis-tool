d31307ba1bd23ad8cf0dd7a5a29127cce3769a88
Mark O'Reilly <marko@megsupporttools.com>
2021-03-16 07:23:01 +0000

    added project paths

--- cms/wsgi.py
+++ cms/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cms.production')
+
+BASE_DIR = os.path.dirname(__file__)
+if BASE_DIR not in sys.path:
+    sys.path.append(BASE_DIR)
+
+ROOT_DIR = "/app"
+if ROOT_DIR not in sys.path:
+    sys.path.append(ROOT_DIR)
 
 application = get_wsgi_application()
