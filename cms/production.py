1834def136d0d27985db1c17851f80108d3e6fd0
Mark O'Reilly <marko@megsupporttools.com>
2021-03-16 07:20:02 +0000

    added hardcoded secret key

--- cms/production.py
+++ cms/production.py
@@ -1,7 +1,7 @@
 from cms.settings import *
 
 DEBUG = False
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = "Eg8BktP1A5pDEsPPeJ7jhIg19SS4vUpR"
 
 SECURE_SSL_REDIRECT = os.environ.get('SECURE_SSL_REDIRECT', True)
 # If True, browsers may ensure that the cookie is only sent under an HTTPS connection
