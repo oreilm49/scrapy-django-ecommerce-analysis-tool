1fe50e7017b0ae191172073cda74a5005f8c0ff3
Mark O'Reilly <marko@megsupporttools.com>
2021-03-17 08:24:24 +0000

    fixed incorrect environment variables

--- cms/production.py
+++ cms/production.py
@@ -5,8 +5,8 @@ SECRET_KEY = "Eg8BktP1A5pDEsPPeJ7jhIg19SS4vUpR"
 
 SECURE_SSL_REDIRECT = os.environ.get('SECURE_SSL_REDIRECT', True)
 # If True, browsers may ensure that the cookie is only sent under an HTTPS connection
-SESSION_COOKIE_SECURE = os.environ.get('SECURE_SSL_REDIRECT', True)
+SESSION_COOKIE_SECURE = os.environ.get('SESSION_COOKIE_SECURE', True)
 # This will ensure that the CSRF Token is only sent over HTTPS
-CSRF_COOKIE_SECURE = os.environ.get('SECURE_SSL_REDIRECT', True)
+CSRF_COOKIE_SECURE = os.environ.get('CSRF_COOKIE_SECURE', True)
 
 SENDGRID_SANDBOX_MODE_IN_DEBUG = False
