7da18e5ef62515cca89825276b98e2b42283ac92
Mark O'Reilly <marko@megsupporttools.com>
2021-03-18 12:23:03 +0000

    added collectstatic to deploy script

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 docker context use default
+docker-compose run cms python3 manage.py collectstatic --noinput
 docker build -t specr.azurecr.io/specr:deployment .
 docker push specr.azurecr.io/specr:deployment
 az container restart --resource-group specr --name specr
