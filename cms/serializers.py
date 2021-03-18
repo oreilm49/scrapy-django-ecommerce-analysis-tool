32f375cdd6a3232553863fa693d5b3c504bebff8
Mark O'Reilly <marko@megsupporttools.com>
2021-03-18 11:07:14 +0000

    added to_float serializer

--- cms/serializers.py
+++ cms/serializers.py
@@ -1,11 +1,22 @@
 from collections import namedtuple
+from typing import Union
 
 from dateutil import parser
 from django import forms
+from pint import UnitRegistry
 
 from cms.constants import FIELD_TYPES
 
 CustomValueSerializer = namedtuple('CustomValueSerializer', ['serializer', 'deserializer'])
+
+
+def to_float(number: Union[str, int, float]):
+    if isinstance(number, float):
+        return number
+    if isinstance(number, int):
+        return float(number)
+    return float(UnitRegistry()(number))
+
 
 serializers = {
     forms.CharField: CustomValueSerializer(
@@ -25,9 +36,8 @@ serializers = {
         deserializer=lambda value: parser.parse(value),
     ),
     forms.FloatField: CustomValueSerializer(
-        serializer=float,
+        serializer=to_float,
         deserializer=float,
     ),
 }
 assert set(serializers.keys()) == set(FIELD_TYPES), 'All allowed field types must have serializer data defined'
-
