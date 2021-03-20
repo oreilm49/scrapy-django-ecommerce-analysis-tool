c8ae7e9948ba55f106a9f8b152039346cd248161
Mark O'Reilly <marko@megsupporttools.com>
2021-03-20 18:14:29 +0000

    wrap deploy scripts in tests if

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-bash ./sh/build.sh
-az container restart --resource-group specr --name specr
+if docker-compose run cms python manage.py test ; then
+    bash ./sh/build.sh
+    az container restart --resource-group specr --name specr
+else
+    echo "Deployment cancelled - tests didn't pass successfully."
+fi
