c8548564e9a74958d54fb0a3486151a32059decf
Mark O'Reilly <marko@megsupporttools.com>
2021-03-20 17:35:15 +0000

    added build script to deploy

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-docker context use default
-docker-compose run cms python3 manage.py collectstatic --noinput
-docker build -t specr.azurecr.io/specr:deployment .
-docker push specr.azurecr.io/specr:deployment
+bash ./sh/build.sh
 az container restart --resource-group specr --name specr
