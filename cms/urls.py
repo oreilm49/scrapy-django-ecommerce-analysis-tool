a3497a382ef40f115949ecd09e62ab0f84363a96
Mark O'Reilly <marko@megsupporttools.com>
2021-03-22 08:14:58 +0000

    added test request to trigger a 500 error

--- cms/urls.py
+++ cms/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.http import HttpResponseServerError
 from django.urls import path, include
 from django.views.i18n import JavaScriptCatalog
 
@@ -9,3 +10,10 @@ urlpatterns = [
     path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+
+def my_test_500_view(request):
+    return HttpResponseServerError()
+
+
+urlpatterns += [path('trigger_500/', my_test_500_view)]
