98a41fbf99aaf39eeb30a53dd781cf30a7df17b2
Mark O'Reilly <marko@megsupporttools.com>
2021-03-23 20:46:58 +0000

    added ProcessFeedback url

--- cms/dashboard/urls.py
+++ cms/dashboard/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from cms.dashboard.views.base import DashboardHome
+from cms.dashboard.views.base import DashboardHome, ProcessFeedback
 from cms.dashboard.views.category_tables import CategoryTables, CategoryTableCreate, CategoryTableDetail, \
     CategoryTableUpdate
 from cms.dashboard.views.products import Products, ProductDetail
@@ -15,4 +15,5 @@ urlpatterns = [
     url(r'category-tables/(?P<pk>\d+)/update$', CategoryTableUpdate.as_view(), name='category-table-update'),
     url(r'products/$', Products.as_view(), name='products'),
     url(r'products/(?P<pk>\d+)/$', ProductDetail.as_view(), name='product'),
+    url(r'feedback/$', ProcessFeedback.as_view(), name='feedback'),
 ]
