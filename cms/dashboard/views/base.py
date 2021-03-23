2a4fc43b9c8b404bcf8bf0a380240e7b88b03eef
Mark O'Reilly <marko@megsupporttools.com>
2021-03-23 20:46:51 +0000

    added ProcessFeedback view

--- cms/dashboard/views/base.py
+++ cms/dashboard/views/base.py
@@ -1,11 +1,18 @@
 from collections import namedtuple
 from typing import List, Optional
 
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.utils.translation import gettext as _
+from django.views import View
 from django.views.generic import RedirectView
 
+from cms.dashboard.forms import FeedbackForm
 from cms.dashboard.toolbar import NavItem
+from cms.settings import ADMINS
+from cms.tasks import send_email
 
 
 class DashboardHome(RedirectView):
@@ -41,6 +48,19 @@ class BaseDashboardMixin(LoginRequiredMixin, BreadcrumbMixin):
             nav_items=(
                 NavItem(label='Pivot', icon='fa fa-table', url=reverse('dashboard:category-tables'), active=reverse('dashboard:category-tables') in self.request.path),
                 NavItem(label='Products', icon='fa fa-shopping-cart', url=reverse('dashboard:products'), active=reverse('dashboard:products') in self.request.path),
-            )
+            ),
+            feedback_form=FeedbackForm()
         )
         return data
+
+
+class ProcessFeedback(LoginRequiredMixin, View):
+
+    def post(self, request):
+        form: FeedbackForm = FeedbackForm(request.POST or None)
+        if form.is_valid():
+            messages.success(request, _('Thanks for the feedback!'))
+            send_email.delay('Feedback', form.cleaned_data['feedback'], [email for name, email in ADMINS])
+        else:
+            messages.error(request, _('There was an error processing your feedback. If this persists, please contact support directly at info@specr.ie'))
+        return HttpResponseRedirect(reverse("dashboard:home"))
