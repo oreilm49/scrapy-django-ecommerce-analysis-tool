12da3339bfac831e5eb0ac70ea21dd7b716bdeb9
Mark O'Reilly <marko@megsupporttools.com>
2021-03-29 19:54:53 +0100

    fixed bug in load_accounts.py

--- cms/accounts/scripts/load_accounts.py
+++ cms/accounts/scripts/load_accounts.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.models import User
 from model_mommy import mommy
 
-from cms.accounts.models import Company, Profile, Team
+from cms.accounts.models import Company, Team
 
 
 def run():
     company, _ = Company.objects.get_or_create(name="Test Company")
-    user: Profile = mommy.make(Profile, company=company, user__username="test user")
-    mommy.make(Team, name="test team", company=company, users=[user.user])
+    user: User = User.objects.create_user("test", password="password")
+    user.profile.company = company
+    user.save()
+    mommy.make(Team, name="test team", company=company, users=[user])
