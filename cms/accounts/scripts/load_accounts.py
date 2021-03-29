5492e584aa4071799bd6e2ef4133eb7bcbad9b51
Mark O'Reilly <marko@megsupporttools.com>
2021-03-29 20:19:26 +0100

    added run function wrapped in atomic transaction

--- cms/accounts/scripts/load_accounts.py
+++ cms/accounts/scripts/load_accounts.py
@@ -1,12 +1,18 @@
 from django.contrib.auth.models import User
+from django.db import transaction
 from model_mommy import mommy
 
 from cms.accounts.models import Company, Team
 
 
-def run():
+def load_accounts():
     company, _ = Company.objects.get_or_create(name="Test Company")
     user: User = User.objects.create_user("test", password="password")
     user.profile.company = company
     user.save()
     mommy.make(Team, name="test team", company=company, users=[user])
+
+
+@transaction.atomic()
+def run():
+    load_accounts()
