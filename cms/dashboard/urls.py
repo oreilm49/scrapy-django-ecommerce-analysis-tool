073cb46296c7cc79bbc23e207d2ecdd90e2abb86
Mark O'Reilly <marko@megsupporttools.com>
2021-03-30 20:22:27 +0100

    added CategoryGapAnalysisReport urls

--- cms/dashboard/urls.py
+++ cms/dashboard/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 
 from cms.dashboard.views.base import DashboardHome, ProcessFeedback
+from cms.dashboard.views.category_gap_analysis import CategoryGapAnalysisReports, CategoryGapAnalysisReportUpdate, \
+    CategoryGapAnalysisReportCreate, CategoryGapAnalysisReportDetail
 from cms.dashboard.views.category_tables import CategoryTables, CategoryTableCreate, CategoryTableDetail, \
     CategoryTableUpdate
 from cms.dashboard.views.products import Products, ProductDetail
@@ -16,4 +18,8 @@ urlpatterns = [
     url(r'products/$', Products.as_view(), name='products'),
     url(r'products/(?P<pk>\d+)/$', ProductDetail.as_view(), name='product'),
     url(r'feedback/$', ProcessFeedback.as_view(), name='feedback'),
+    url(r'category-gap-reports/$', CategoryGapAnalysisReports.as_view(), name='category-gap-reports'),
+    url(r'category-gap-reports/add/$', CategoryGapAnalysisReportCreate.as_view(), name='category-gap-report-create'),
+    url(r'category-gap-reports/(?P<pk>\d+)$', CategoryGapAnalysisReportDetail.as_view(), name='category-gap-report'),
+    url(r'category-gap-reports/(?P<pk>\d+)/update$', CategoryGapAnalysisReportUpdate.as_view(), name='category-gap-report-update'),
 ]
