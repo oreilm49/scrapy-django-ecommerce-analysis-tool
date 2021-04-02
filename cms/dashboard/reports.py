15260990860f2f85948fe60c28b31b813a833c9a
Mark O'Reilly <marko@megsupporttools.com>
2021-04-02 12:49:16 +0100

    added get_product_spec_values logic and base dominance methods

--- cms/dashboard/reports.py
+++ cms/dashboard/reports.py
@@ -14,7 +14,22 @@ class ProductCluster:
         self.products = products
         self.category = category
 
-    def dominant_specs(self):
+    def get_product_spec_values(self) -> List[Dict[str, Union[str, float, int, bool]]]:
+        """Returns a list of dicts of the spec values for each product."""
+        spec_values: List[Dict[str, Union[str, float, int, bool]]] = []
+        for product in self.products:
+            assert product.category == self.category, f"Product used from incorrect category: {product.category}."
+            product_specs = {}
+            for attribute_config in product.category.category_attribute_configs.order_by('order').iterator():
+                attribute_config: 'CategoryAttributeConfig'
+                product_attr: ProductAttribute = attribute_config.attribute_type.productattributes.filter(product__pk=product.pk).first()
+                if product_attr:
+                    product_specs[attribute_config.attribute_type.name] = product_attr.data['value']
+            if product_specs:
+                spec_values.append(product_specs)
+        return spec_values
+
+    def dominant_specs(self) -> Dict[str, Dict[str, Union[int, float, str]]]:
         """Gets the most common spec combinations for this pricepoint"""
         return
 
