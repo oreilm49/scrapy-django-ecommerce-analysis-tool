39bbad4b48afb0f6fae71fdac3dbb1c3e6331054
Mark O'Reilly <marko@megsupporttools.com>
2021-04-02 13:52:52 +0100

    tested cluster average price logic

--- cms/dashboard/tests/test_reports.py
+++ cms/dashboard/tests/test_reports.py
@@ -4,7 +4,8 @@ from django.test import TestCase
 from model_mommy import mommy
 
 from cms.dashboard.reports import ProductCluster
-from cms.models import Product, ProductAttribute, Category, CategoryAttributeConfig, AttributeType
+from cms.models import Product, ProductAttribute, Category, CategoryAttributeConfig, AttributeType, \
+    WebsiteProductAttribute
 
 
 class TestReports(TestCase):
@@ -18,20 +19,22 @@ class TestReports(TestCase):
         cls.cat_cfg_2: CategoryAttributeConfig = mommy.make(CategoryAttributeConfig, attribute_type__name="spin", order=2, category=cls.category)
         cls.cat_cfg_3: CategoryAttributeConfig = mommy.make(CategoryAttributeConfig, attribute_type__name="energy usage", order=3, category=cls.category)
         cls.brand_attr: AttributeType = mommy.make(AttributeType, name="brand")
+        cls.price_attr: AttributeType = mommy.make(AttributeType, name="price")
 
-        def make_product(load_size: int, spin: int, energy: int, order: int, brand: Optional[str] = None) -> Product:
+        def make_product(price: float, load_size: int, spin: int, energy: int, order: int, brand: Optional[str] = None) -> Product:
             product: Product = mommy.make(Product, category=cls.category, order=order)
             mommy.make(ProductAttribute, product=product, attribute_type=cls.cat_cfg_1.attribute_type, data={'value': load_size})
             mommy.make(ProductAttribute, product=product, attribute_type=cls.cat_cfg_2.attribute_type, data={'value': spin})
             mommy.make(ProductAttribute, product=product, attribute_type=cls.cat_cfg_3.attribute_type, data={'value': energy})
             if brand:
                 mommy.make(ProductAttribute, product=product, attribute_type=cls.brand_attr, data={'value': brand})
+            mommy.make(WebsiteProductAttribute, product=product, attribute_type=cls.price_attr, data={'value': price})
             return product
 
-        make_product(7, 1400, 50, 1, brand="whirlpool")
-        make_product(8, 1400, 75, 2, brand="whirlpool")
-        make_product(8, 1600, 100, 3, brand="hotpoint")
-        make_product(6, 1200, 100, 4, brand="indesit")
+        make_product(299.99, 7, 1400, 50, 1, brand="whirlpool")
+        make_product(399.99, 8, 1400, 75, 2, brand="whirlpool")
+        make_product(349.99, 8, 1600, 100, 3, brand="hotpoint")
+        make_product(399.99, 6, 1200, 100, 4, brand="indesit")
 
     def test_product_cluster_get_product_spec_values(self):
         with self.subTest("incorrect category for products"):
@@ -66,7 +69,7 @@ class TestReports(TestCase):
             product_spec_values = cluster.get_product_spec_values()
             self.assertEqual(product_spec_values, [])
 
-    def test_dominant_specs(self):
+    def test_product_cluster_dominant_specs(self):
         cluster: ProductCluster = ProductCluster(self.category, Product.objects.filter(category=self.category).order_by('order'))
         dominant_specs = cluster.dominant_specs()
         self.assertEqual(dominant_specs['load size'], {'value': 8, 'number_of_products': 2})
@@ -77,10 +80,18 @@ class TestReports(TestCase):
             cluster: ProductCluster = ProductCluster(self.category, Product.objects.none())
             self.assertEqual(cluster.dominant_specs(), {})
 
-    def test_dominant_brands(self):
+    def test_product_cluster_dominant_brands(self):
         cluster: ProductCluster = ProductCluster(self.category, Product.objects.filter(category=self.category).order_by('order'))
         self.assertEqual(cluster.dominant_brands(), {'value': "whirlpool", 'number_of_products': 2})
 
         with self.subTest("empty queryset"):
             cluster: ProductCluster = ProductCluster(self.category, Product.objects.none())
             self.assertEqual(cluster.dominant_brands(), {})
+
+    def test_product_cluster_average_price(self):
+        cluster: ProductCluster = ProductCluster(self.category, Product.objects.filter(category=self.category).order_by('order'))
+        self.assertEqual(cluster.average_price(), 361)
+
+        with self.subTest("empty queryset"):
+            cluster: ProductCluster = ProductCluster(self.category, Product.objects.none())
+            self.assertEqual(cluster.average_price(), None)
