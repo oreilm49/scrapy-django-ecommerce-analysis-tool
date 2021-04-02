7fcfc20ceeab5324bb9727fcb6e0c5ede874639f
Mark O'Reilly <marko@megsupporttools.com>
2021-04-02 12:48:20 +0100

    added report tests

---
+++ cms/dashboard/tests/test_reports.py
@@ -0,0 +1,62 @@
+from django.test import TestCase
+from model_mommy import mommy
+
+from cms.dashboard.reports import ProductCluster
+from cms.models import Product, ProductAttribute, Category, CategoryAttributeConfig
+
+
+class TestReports(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        # set up category
+        cls.category: Category = mommy.make(Category, name="washers")
+        cls.cat_cfg_1: CategoryAttributeConfig = mommy.make(CategoryAttributeConfig, attribute_type__name="load size", order=1, category=cls.category)
+        cls.cat_cfg_2: CategoryAttributeConfig = mommy.make(CategoryAttributeConfig, attribute_type__name="spin", order=2, category=cls.category)
+        cls.cat_cfg_3: CategoryAttributeConfig = mommy.make(CategoryAttributeConfig, attribute_type__name="energy usage", order=3, category=cls.category)
+
+        def make_product(load_size: int, spin: int, energy: int, order: int) -> Product:
+            product: Product = mommy.make(Product, category=cls.category, order=order)
+            mommy.make(ProductAttribute, product=product, attribute_type=cls.cat_cfg_1.attribute_type, data={'value': load_size})
+            mommy.make(ProductAttribute, product=product, attribute_type=cls.cat_cfg_2.attribute_type, data={'value': spin})
+            mommy.make(ProductAttribute, product=product, attribute_type=cls.cat_cfg_3.attribute_type, data={'value': energy})
+            return product
+
+        make_product(7, 1400, 50, 1)
+        make_product(8, 1400, 75, 2)
+        make_product(8, 1600, 100, 3)
+        make_product(6, 1200, 100, 4)
+
+    def test_product_cluster_get_product_spec_values(self):
+        with self.subTest("incorrect category for products"):
+            cluster: ProductCluster = ProductCluster(mommy.make(Category), [mommy.make(Product, category__name="test")])
+            with self.assertRaises(AssertionError) as context:
+                cluster.get_product_spec_values()
+            self.assertEqual(str(context.exception), "Product used from incorrect category: test.")
+
+        with self.subTest("empty product queryset"):
+            cluster: ProductCluster = ProductCluster(self.category, Product.objects.none())
+            product_spec_values = cluster.get_product_spec_values()
+            self.assertEqual(product_spec_values, [])
+
+        with self.subTest("spec values"):
+            cluster: ProductCluster = ProductCluster(self.category, Product.objects.filter(category=self.category).order_by('order'))
+            product_spec_values = cluster.get_product_spec_values()
+            self.assertIsInstance(product_spec_values, list)
+            self.assertEqual(product_spec_values[0], {
+                'load size': 7,
+                'spin': 1400,
+                'energy usage': 50,
+            })
+            self.assertEqual(product_spec_values[-1], {
+                'load size': 6,
+                'spin': 1200,
+                'energy usage': 100,
+            })
+
+        with self.subTest("No spec values"):
+            ProductAttribute.objects.all().delete()
+            cluster: ProductCluster = ProductCluster(self.category, Product.objects.filter(category=self.category).order_by('order'))
+            product_spec_values = cluster.get_product_spec_values()
+            self.assertEqual(product_spec_values, [])
