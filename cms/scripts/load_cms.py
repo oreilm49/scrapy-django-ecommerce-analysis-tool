0b3baff274a9ec602eb3fd2a05d1785c308e6f7b
Mark O'Reilly <marko@megsupporttools.com>
2021-04-05 11:22:09 +0100

    added code to load list of products in load_cms

--- cms/scripts/load_cms.py
+++ cms/scripts/load_cms.py
@@ -3,8 +3,55 @@ from django.db import transaction
 from cms.constants import CATEGORY, TABLE_LABEL_COLUMN, TABLE_VALUE_COLUMN, TABLE, LINK, PAGINATION, PRICE, MODEL, \
     HOURLY, IMAGE
 from cms.form_widgets import FloatInput
-from cms.models import Website, Category, Url, Selector, Unit
+from cms.models import Website, Category, Url, Selector, Unit, Product, ProductAttribute, AttributeType, \
+    WebsiteProductAttribute
 from cms.utils import get_dotted_path
+
+
+products = [
+    ["P35106SKW", "6kg", "1000", "71", "209.95", "powerpoint"],
+    ["WM1003WH", "5kg", "1000", "75", "209.95", "nordmende"],
+    ["WTK62051W", "6kg", "1200", "75", "219.95", "beko"],
+    ["P35127SKW", "7kg", "1200", "80", "229.99", "powerpoint"],
+    ["WTL72051W", "7kg", "1200", "80", "229.95", "beko"],
+    ["WTL82051W", "8kg", "1200", "80", "229.95", "beko"],
+    ["IWC71452WUKN", "7kg", "1400", "95", "249.99", "indesit"],
+    ["WM1477WH", "7kg", "1400", "95", "279.95", "nordmende"],
+    ["CS 148TE-80", "8kg", "1400", "95", "289.95", "candy"],
+    ["WM1277BL", "7kg", "1200", "85", "289.95", "nordmende"],
+    ["H3W 49TE-80", "9kg", "1400", "100", "299.99", "hoover"],
+    ["WEY86052W", "8kg", "1600", "100", "299.99", "beko"],
+    ["WTG941B3W", "9kg", "1400", "120", "299.99", "beko"],
+    ["IWC91482ECO", "9kg", "1400", "110", "299.99", "indesit"],
+    ["MTWC91483WUK", "9kg", "1400", "109", "299.99", "indesit"],
+    ["WTL74051B", "7kg", "1400", "100", "299.99", "beko"],
+    ["WMT1270BL", "7kg", "1200", "105", "329.99", "beko"],
+]
+
+
+def set_up_attributes():
+    load_size_unit, _ = Unit.objects.get_or_create(name="kilogram", widget=get_dotted_path(FloatInput))
+    spin_unit, _ = Unit.objects.get_or_create(name="revolutions_per_minute", widget=get_dotted_path(FloatInput))
+    energy_unit, _ = Unit.objects.get_or_create(name="kwh", widget=get_dotted_path(FloatInput))
+    currency, _ = Unit.objects.get_or_create(name="€", alternate_names=["euro"], widget=get_dotted_path(FloatInput), repeat=HOURLY)
+    AttributeType.objects.get_or_create(name="brand")
+    AttributeType.objects.get_or_create(name="load size", unit=load_size_unit)
+    AttributeType.objects.get_or_create(name="spin speed", unit=spin_unit)
+    AttributeType.objects.get_or_create(name="energy usage", unit=energy_unit)
+    AttributeType.objects.get_or_create(name="price", unit=currency)
+
+
+def make_product(model: str, price: float, load_size: int, spin: int, energy: int, category: Category, brand: str) -> Product:
+    if Product.objects.filter(model=model).exists():
+        return
+    product = Product.objects.create(model=model, category=category)
+    ProductAttribute.objects.create(product=product, attribute_type=AttributeType.objects.get(name="load size"), data={'value': load_size})
+    ProductAttribute.objects.create(product=product, attribute_type=AttributeType.objects.get(name="spin speed"), data={'value': spin})
+    ProductAttribute.objects.create(product=product, attribute_type=AttributeType.objects.get(name="energy usage"), data={'value': energy})
+    ProductAttribute.objects.create(product=product, attribute_type=AttributeType.objects.get(name="brand"), data={'value': brand})
+    website = Website.objects.get(name="harvey_norman")
+    WebsiteProductAttribute.objects.create(product=product, website=website, attribute_type=AttributeType.objects.get(name="price"), data={'value': price})
+    return product
 
 
 def set_up_websites():
@@ -24,6 +71,14 @@ def set_up_websites():
     Selector.objects.get_or_create(selector_type=IMAGE, css_selector='.cm-image-previewer.image-magnifier-image::attr(href)', website=harvey_norman)
 
 
+def set_up_products():
+    category = Category.objects.get(name="washing machines")
+    for product in products:
+        make_product(product[0], product[4], product[1], product[2], product[3], category, product[5])
+
+
 @transaction.atomic()
 def run():
     set_up_websites()
+    set_up_attributes()
+    set_up_products()
