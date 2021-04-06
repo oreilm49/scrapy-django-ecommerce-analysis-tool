54d2598d61812f6d669372dc50911371f1b93fe8
Mark O'Reilly <marko@megsupporttools.com>
2021-04-06 08:03:24 +0100

    added tag widget

--- cms/form_widgets.py
+++ cms/form_widgets.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms.widgets import Input
 
 
@@ -8,3 +9,13 @@ class FloatInput(Input):
         self.attrs = {'step': '0.1'}
         self.template_name = 'django/forms/widgets/number.html'
         super(FloatInput, self).__init__()
+
+
+class TagWidget(forms.widgets.TextInput):
+    template_name = 'widgets/tag.html'
+
+    class Media:
+        js = ('js/jquery.amsify.suggestags.js', 'js/inittags.js')
+        css = {
+            'all': ('css/amsify.suggestags.css',)
+        }
