ea67b3c954f69cd23b3927e8ffb20b6df161221a
Mark O'Reilly <marko@megsupporttools.com>
2021-04-09 08:24:12 +0100

    added test for image processing

---
+++ cms/data_processing/tests/test_image_processing.py
@@ -0,0 +1,36 @@
+import shutil
+
+import requests
+import tempfile
+from django.test import TestCase, override_settings
+
+from cms.data_processing.image_processing import small_pdf_2_image
+from cms.scraper.settings import IMAGES_ENERGY_LABELS_STORE
+
+
+MEDIA_ROOT = tempfile.mkdtemp()
+
+
+@override_settings(MEDIA_ROOT=MEDIA_ROOT)
+class TestUnits(TestCase):
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(MEDIA_ROOT, ignore_errors=True)
+        super().tearDownClass()
+
+    def test_small_pdf_2_image(self):
+        pdf_url = "https://cc.isitetv.com/clients/wp/whirlpool/859991530490/documentation/eur/whirlpool-fwl71253wuk-energy-label-el859991530490.pdf"
+        with self.subTest("url valid"):
+            self.assertEqual(requests.get(pdf_url).status_code, 200)
+
+        with self.subTest("valid pdf"):
+            image_path = small_pdf_2_image(pdf_url)
+            self.assertIn(IMAGES_ENERGY_LABELS_STORE, image_path)
+            self.assertIn('.png', image_path)
+            open(image_path)
+
+        with self.subTest("pdf file validation"):
+            with self.assertRaises(Exception) as context:
+                small_pdf_2_image("test.jpg")
+            self.assertEqual(str(context.exception), "Download url is not a pdf link: 'test.jpg'")
