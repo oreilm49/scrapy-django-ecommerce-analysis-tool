03c2be63cd990354fc38e7531bfef0863c7faced
Mark O'Reilly <marko@megsupporttools.com>
2021-04-10 08:49:35 +0100

    tested cropping label

--- cms/data_processing/tests/test_image_processing.py
+++ cms/data_processing/tests/test_image_processing.py
@@ -1,18 +1,19 @@
+import os
 import shutil
 
 import requests
 from django.test import TestCase
 
-from cms.data_processing.image_processing import small_pdf_2_image
+from cms.data_processing.image_processing import small_pdf_2_image, energy_label_cropped_2_qr
 from cms.scraper.settings import IMAGES_ENERGY_LABELS_STORE
 
 
-class TestUnits(TestCase):
+class TestImageProcessing(TestCase):
 
     @classmethod
-    def tearDownClass(cls):
-        shutil.rmtree(IMAGES_ENERGY_LABELS_STORE)
-        super().tearDownClass()
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.image_path = "/app/cms/data_processing/tests/data/label.png"
 
     def test_small_pdf_2_image(self):
         pdf_url = "https://cc.isitetv.com/clients/wp/whirlpool/859991530490/documentation/eur/whirlpool-fwl71253wuk-energy-label-el859991530490.pdf"
@@ -29,3 +30,10 @@ class TestUnits(TestCase):
             with self.assertRaises(Exception) as context:
                 small_pdf_2_image("test.jpg")
             self.assertEqual(str(context.exception), "Download url is not a pdf link: 'test.jpg'")
+        shutil.rmtree(IMAGES_ENERGY_LABELS_STORE)
+
+    def test_energy_label_cropped_2_qr(self):
+        cropped_image_path = energy_label_cropped_2_qr(self.image_path)
+        self.assertEqual("/app/cms/data_processing/tests/data/label_qr.png", cropped_image_path)
+        open(cropped_image_path)
+        os.remove(cropped_image_path)
