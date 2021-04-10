d00b47f14ed614563df0ed3a35683c403f713db6
Mark O'Reilly <marko@megsupporttools.com>
2021-04-10 13:02:26 +0100

    used create_product_attribute method

--- cms/scraper/pipelines.py
+++ cms/scraper/pipelines.py
@@ -29,27 +29,7 @@ class ProductAttributePipeline:
             product: Product = item['product']
             for attribute in item['attributes']:
                 attribute: Dict
-                attribute_type: AttributeType = AttributeType.objects.custom_get_or_create(attribute['label'])
-                product_attribute_exists: bool = ProductAttribute.objects.filter(attribute_type=attribute_type, product=product).exists()
-                if product_attribute_exists:
-                    continue
-
-                processed_unit: Union[UnitValue, Value, RangeUnitValue] = UnitManager().get_processed_unit_and_value(attribute['value'], unit=attribute_type.unit)
-                if not attribute_type.unit and isinstance(processed_unit, (UnitValue, RangeUnitValue)):
-                    attribute_type.unit = processed_unit.unit
-                    attribute_type.save()
-                if isinstance(processed_unit, RangeUnitValue):
-                    name_low: str = f"{attribute_type.name} - low"
-                    name_high: str = f"{attribute_type.name} - high"
-                    if ProductAttribute.objects.filter(Q(attribute_type__name=name_low) | Q(attribute_type__name=name_high), product=product).exists():
-                        continue
-
-                    attribute_type_low: AttributeType = AttributeType.objects.custom_get_or_create(f"{attribute['label']} - low", unit=processed_unit.unit)
-                    attribute_type_high: AttributeType = AttributeType.objects.custom_get_or_create(f"{attribute['label']} - high", unit=processed_unit.unit)
-                    ProductAttribute.objects.custom_get_or_create(product=product, attribute_type=attribute_type_low, value=processed_unit.value_low)
-                    ProductAttribute.objects.custom_get_or_create(product=product, attribute_type=attribute_type_high, value=processed_unit.value_high)
-                else:
-                    ProductAttribute.objects.custom_get_or_create(product=product, attribute_type=attribute_type, value=processed_unit.value)
+                product.create_product_attribute(attribute['label'], attribute['value'])
         return item
 
 
