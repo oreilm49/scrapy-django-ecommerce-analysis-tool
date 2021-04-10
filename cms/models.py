0528b42db3049c2aa0965d209bdae96600ee7300
Mark O'Reilly <marko@megsupporttools.com>
2021-04-10 13:02:16 +0100

    added create_product_attribute and get_eprel_attribute_data methods

--- cms/models.py
+++ cms/models.py
@@ -242,6 +242,42 @@ class Product(BaseModel):
                 self.save()
                 return url
 
+    @transaction.atomic
+    def get_eprel_attribute_data(self) -> None:
+        url: Optional[str] = self.get_eprel_api_url()
+        if not url or self.eprel_scraped:
+            return
+        response = requests.get(url)
+        for attribute_label, attribute_value in response.json():
+            attribute_label: str = camel_case_to_sentence(attribute_label)
+            if isinstance(attribute_value, (int, float, str)):
+                self.create_product_attribute(attribute_label, attribute_value)
+        self.eprel_scraped = True
+        self.save()
+
+    def create_product_attribute(self, attribute_label: str, attribute_value: Union[str, int, float]) -> None:
+        attribute_type: AttributeType = AttributeType.objects.custom_get_or_create(attribute_label)
+        product_attribute_exists: bool = ProductAttribute.objects.filter(attribute_type=attribute_type, product=self).exists()
+        if product_attribute_exists:
+            return
+
+        processed_unit: Union[UnitValue, Value, RangeUnitValue] = UnitManager().get_processed_unit_and_value(attribute_value, unit=attribute_type.unit)
+        if not attribute_type.unit and isinstance(processed_unit, (UnitValue, RangeUnitValue)):
+            attribute_type.unit = processed_unit.unit
+            attribute_type.save()
+        if isinstance(processed_unit, RangeUnitValue):
+            name_low: str = f"{attribute_type.name} - low"
+            name_high: str = f"{attribute_type.name} - high"
+            if ProductAttribute.objects.filter(Q(attribute_type__name=name_low) | Q(attribute_type__name=name_high), product=self).exists():
+                return
+
+            attribute_type_low: AttributeType = AttributeType.objects.custom_get_or_create(f"{attribute_label} - low", unit=processed_unit.unit)
+            attribute_type_high: AttributeType = AttributeType.objects.custom_get_or_create(f"{attribute_label} - high", unit=processed_unit.unit)
+            ProductAttribute.objects.custom_get_or_create(product=self, attribute_type=attribute_type_low, value=processed_unit.value_low)
+            ProductAttribute.objects.custom_get_or_create(product=self, attribute_type=attribute_type_high, value=processed_unit.value_high)
+        else:
+            ProductAttribute.objects.custom_get_or_create(product=self, attribute_type=attribute_type, value=processed_unit.value)
+
 
 class AttributeTypeQuerySet(BaseQuerySet):
 
