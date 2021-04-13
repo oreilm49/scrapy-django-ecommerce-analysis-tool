207c895f61ece505a41ae8c22fa8d9ac947c6d29
Mark O'Reilly <marko@megsupporttools.com>
2021-04-13 07:40:13 +0100

    only show product if grouper exists

--- cms/dashboard/views/category_tables.py
+++ cms/dashboard/views/category_tables.py
@@ -146,7 +146,7 @@ class CategoryTableDetail(BaseDashboardMixin, DetailView):
             product=product
         ) for product in self.table.get_products(Product.objects.published())]
         y_axis_groups: Iterator[Tuple] = itertools.groupby(
-            sorted(products, key=lambda product: product.y_axis_grouper),
+            sorted([product for product in products if product.y_axis_grouper], key=lambda product: product.y_axis_grouper),
             key=lambda product: product.y_axis_grouper
         )
         table_data = {}
