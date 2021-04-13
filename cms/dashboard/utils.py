cb2220d47410564fd3a4e68cfab7ee3f2a71a084
Mark O'Reilly <marko@megsupporttools.com>
2021-04-13 07:15:08 +0100

    dont build chart if no data

--- cms/dashboard/utils.py
+++ cms/dashboard/utils.py
@@ -14,7 +14,9 @@ def get_brands() -> List[Optional[str]]:
     return ProductAttribute.objects.filter(attribute_type__name="brand").values_list('data__value', flat=True).distinct()
 
 
-def line_chart(df: DataFrame, title: str, x: str, x_label: str, y: str, y_label: str) -> Dict:
+def line_chart(df: DataFrame, title: str, x: str, x_label: str, y: str, y_label: str) -> Optional[Dict]:
+    if not df:
+        return None
     plot = figure(title=title, x_axis_label=x_label, y_axis_label=y_label, sizing_mode="stretch_both")
     plot.line(x=x, y=y, line_width=2, source=ColumnDataSource(df))
     script, div = components(plot)
