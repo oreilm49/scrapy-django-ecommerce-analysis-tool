5ea1665f822912c666cbbf9936a1d869ed5ad0da
Mark O'Reilly <marko@megsupporttools.com>
2021-04-15 07:47:32 +0100

    added catgory table specs url

--- cms/dashboard/urls.py
+++ cms/dashboard/urls.py
@@ -4,7 +4,7 @@ from cms.dashboard.views.base import DashboardHome, ProcessFeedback
 from cms.dashboard.views.category_gap_analysis import CategoryGapAnalysisReports, CategoryGapAnalysisReportUpdate, \
     CategoryGapAnalysisReportCreate, CategoryGapAnalysisReportDetail
 from cms.dashboard.views.category_tables import CategoryTables, CategoryTableCreate, CategoryTableDetail, \
-    CategoryTableUpdate
+    CategoryTableUpdate, CategoryTableAttributeUpdate
 from cms.dashboard.views.products import Products, ProductDetail
 
 app_name = 'dashboard'
@@ -15,6 +15,7 @@ urlpatterns = [
     url(r'category-tables/add/$', CategoryTableCreate.as_view(), name='category-table-create'),
     url(r'category-tables/(?P<pk>\d+)/$', CategoryTableDetail.as_view(), name='category-table'),
     url(r'category-tables/(?P<pk>\d+)/update$', CategoryTableUpdate.as_view(), name='category-table-update'),
+    url(r'category-tables/(?P<pk>\d+)/specs$', CategoryTableAttributeUpdate.as_view(), name='category-table-specs'),
     url(r'products/$', Products.as_view(), name='products'),
     url(r'products/(?P<pk>\d+)/$', ProductDetail.as_view(), name='product'),
     url(r'feedback/$', ProcessFeedback.as_view(), name='feedback'),
