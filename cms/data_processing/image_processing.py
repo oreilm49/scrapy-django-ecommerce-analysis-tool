05836567ff51224ab2632ba6477545209af39e84
Mark O'Reilly <marko@megsupporttools.com>
2021-04-17 11:11:14 +0100

    handle if pdf link is actually an image

--- cms/data_processing/image_processing.py
+++ cms/data_processing/image_processing.py
@@ -12,6 +12,14 @@ import uuid
 from cms.scraper.settings import IMAGES_ENERGY_LABELS_STORE
 
 
+class ContentTypeImageException(Exception):
+    """Raised when a pdf is expected, but got an image"""
+
+    def __init__(self, content_type: str, *args: object) -> None:
+        super().__init__(*args)
+        self.content_type = content_type
+
+
 def validate_pdf_url(download_url: str) -> str:
     """
     validates if download_url is a valid pdf.
@@ -19,6 +27,8 @@ def validate_pdf_url(download_url: str) -> str:
     response = requests.head(download_url)
     if response.status_code != 200:
         raise Exception(f"Download url return invalid response: '{download_url}' -> '{response.status_code}'")
+    if response.headers['Content-Type'] in ['image/png', 'image/jpeg']:
+        raise ContentTypeImageException(response.headers['Content-Type'])
     if response.headers['Content-Type'] != 'application/pdf':
         raise Exception(f"Download url is not a pdf link: '{download_url}'")
     return download_url
@@ -29,16 +39,28 @@ def small_pdf_2_image(download_url: str) -> str:
     converts pdf url to image and saves in images folder
     returns path to image
     """
-    download_url: str = validate_pdf_url(download_url)
     storage_folder_exists = os.path.isdir(IMAGES_ENERGY_LABELS_STORE)
     if not storage_folder_exists:
         os.makedirs(IMAGES_ENERGY_LABELS_STORE)
+    try:
+        download_url: str = validate_pdf_url(download_url)
+    except ContentTypeImageException:
+        return download_image(download_url)
     with tempfile.TemporaryDirectory() as path:
         images = convert_from_path(download_url, output_folder=path)
         for image in images:
             path: str = f"{IMAGES_ENERGY_LABELS_STORE}/{uuid.uuid4()}.png"
             image.save(path)
             return path
+
+
+def download_image(url: str) -> str:
+    response = requests.get(url)
+    image_data = response.content
+    path: str = f"{IMAGES_ENERGY_LABELS_STORE}/{uuid.uuid4()}.{response.headers['Content-Type'].replace('image/', '')}"
+    with open(path, 'wb') as handler:
+        handler.write(image_data)
+    return path
 
 
 def energy_label_cropped_2_qr(image_path: str) -> str:
