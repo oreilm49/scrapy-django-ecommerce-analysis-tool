edf7a21db6b9ba5f84ed22d1eace00aa665f6985
Mark O'Reilly <marko@megsupporttools.com>
2021-04-17 16:55:44 +0100

    fixed bug in template tag

--- cms/dashboard/templatetags/dashboard_extras.py
+++ cms/dashboard/templatetags/dashboard_extras.py
@@ -33,7 +33,7 @@ def product_specs(context: dict, product: Product):
 
 @register.simple_tag(takes_context=True)
 def product_attribute(context: dict, product_attribute: ProductAttribute):
-    if product_attribute.attribute_type.unit:
+    if product_attribute.attribute_type and product_attribute.attribute_type.unit:
         if product_attribute.attribute_type.unit.is_bool:
             html = '<i class="fa fa-check-circle text-success" aria-hidden="true"></i>'
         else:
