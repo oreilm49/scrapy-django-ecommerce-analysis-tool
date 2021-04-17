3e4da2ef2c793cd43f2e0e959cabf0ab1a6c9d9c
Mark O'Reilly <marko@megsupporttools.com>
2021-04-17 14:02:18 +0100

    added password reset views

--- cms/urls.py
+++ cms/urls.py
@@ -1,6 +1,8 @@
+import debug_toolbar
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.views.i18n import JavaScriptCatalog
 
@@ -9,8 +11,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('cms.dashboard.urls', namespace='dashboard')),
     path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
-    path('accounts/', include('django.contrib.auth.urls')),
     path('api/v1/', include('cms.api.urls', namespace='api')),
+    path('__debug__/', include(debug_toolbar.urls)),
+    path('accounts/login/', auth_views.LoginView.as_view()),
+    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(template_name='auth/password_reset_form.html')),
+    path('accounts/password_reset/done/', auth_views.PasswordChangeDoneView.as_view(template_name='auth/password_reset_done.html')),
+    path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='auth/password_reset_confirm.html')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 if settings.DEBUG:
