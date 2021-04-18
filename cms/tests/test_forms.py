96552903b38e74034f9e2d51639d949ab85b6ccc
Mark O'Reilly <marko@megsupporttools.com>
2021-04-18 10:31:02 +0100

    tested AttributeTypeForm

--- cms/tests/test_forms.py
+++ cms/tests/test_forms.py
@@ -2,9 +2,9 @@ from django.test import TestCase
 from model_mommy import mommy
 
 from cms.constants import MAIN, THUMBNAIL
-from cms.models import Category, Product, ProductAttribute, Website, WebsiteProductAttribute, AttributeType,\
-    ProductImage
-from cms.forms import ProductMergeForm, AttributeTypeMergeForm
+from cms.models import Category, Product, ProductAttribute, Website, WebsiteProductAttribute, AttributeType, \
+    ProductImage, Unit
+from cms.forms import ProductMergeForm, AttributeTypeMergeForm, AttributeTypeForm
 
 
 class TestForms(TestCase):
@@ -15,7 +15,8 @@ class TestForms(TestCase):
         cls.website: Website = mommy.make(Website, name="test_website", currency__name="€")
         cls.category: Category = mommy.make(Category, name="washing machines")
         cls.product: Product = mommy.make(Product, model="model_number")
-        cls.attribute: AttributeType = mommy.make(AttributeType, name="test_attr", unit__name="gram")
+        cls.gram: Unit = mommy.make(Unit, name="gram")
+        cls.attribute: AttributeType = mommy.make(AttributeType, name="test_attr", unit=cls.gram)
         cls.product_attribute: ProductAttribute = mommy.make(ProductAttribute, product=cls.product, attribute_type=cls.attribute)
 
     def test_product_merge_form(self):
@@ -56,3 +57,47 @@ class TestForms(TestCase):
         self.assertEqual(ProductAttribute.objects.get(pk=product_attr__mapped.pk).attribute_type, self.attribute)
         self.assertIsNotNone(AttributeType.objects.get(pk=self.attribute.pk).unit)
         self.assertEqual(WebsiteProductAttribute.objects.get(pk=web_attr__mapped.pk).attribute_type, self.attribute)
+
+    def test_attribute_type_form__unit_conversion(self):
+        kilogram: Unit = mommy.make(Unit, name="kilogram")
+        attribute_type: AttributeType = mommy.make(AttributeType, name="weight", unit=kilogram)
+        form: AttributeTypeForm = AttributeTypeForm(data={'name': 'weight', 'unit': kilogram, 'alternate_names': []}, instance=attribute_type)
+
+        with self.subTest("same unit type"):
+            self.assertTrue(form.is_valid(), msg=form.errors)
+            form.save()
+            self.assertEqual(AttributeType.objects.get(pk=attribute_type.pk).unit, kilogram)
+
+        mommy.make(ProductAttribute, attribute_type=attribute_type, data={'value': 7})
+        with self.subTest("convert to grams"):
+            form: AttributeTypeForm = AttributeTypeForm({'name': 'weight', 'unit': self.gram, 'alternate_names': []}, instance=attribute_type)
+            self.assertTrue(form.is_valid(), msg=form.errors)
+            form.save()
+            self.assertEqual(AttributeType.objects.get(pk=attribute_type.pk).unit, self.gram)
+            self.assertTrue(ProductAttribute.objects.filter(attribute_type=attribute_type, data__value=7000))
+
+        with self.subTest("unit is None"):
+            attribute_type_1: AttributeType = mommy.make(AttributeType, name="test1", unit=None)
+            attribute_type_2: AttributeType = mommy.make(AttributeType, name="test2", unit=None)
+            form: AttributeTypeForm = AttributeTypeForm({'name': 'test1', 'unit': attribute_type_2.unit}, instance=attribute_type_1)
+            self.assertTrue(form.is_valid(), msg=form.errors)
+            form.save()
+            self.assertIsNone(AttributeType.objects.get(pk=attribute_type_1.pk).unit)
+
+        with self.subTest("mismatch units"):
+            litre: Unit = mommy.make(Unit, name="litre")
+            mommy.make(ProductAttribute, attribute_type=attribute_type, data={'value': 7})
+            form: AttributeTypeForm = AttributeTypeForm({'name': 'weight', 'unit': litre}, instance=attribute_type)
+            self.assertFalse(form.is_valid())
+            self.assertIn("Cannot convert from 'gram' ([mass]) to 'liter' ([length] ** 3)", form.errors['unit'])
+            self.assertTrue(ProductAttribute.objects.filter(attribute_type=attribute_type, attribute_type__unit=self.gram, data__value=7))
+
+        with self.subTest("text conversion attempt"):
+            mg: Unit = mommy.make(Unit, name="mg")
+            mommy.make(ProductAttribute, attribute_type=attribute_type, data={'value': 'test'})
+            form: AttributeTypeForm = AttributeTypeForm({'name': 'weight', 'unit': mg}, instance=attribute_type)
+            self.assertTrue(form.is_valid(), msg=form.errors)
+            with self.assertRaises(Exception) as context:
+                form.save()
+            self.assertIn("'test' is not defined in the unit registry", str(context.exception))
+            self.assertTrue(ProductAttribute.objects.filter(attribute_type=attribute_type, attribute_type__unit=self.gram, data__value='test'))
