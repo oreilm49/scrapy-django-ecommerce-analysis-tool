53247876a0557fa68b1815e9a632611b8a7beeb8
Mark O'Reilly <marko@megsupporttools.com>
2021-04-20 19:29:37 +0100

    added ProductInlineAdmin to brand

--- cms/admin.py
+++ cms/admin.py
@@ -73,6 +73,14 @@ class ProductImageInlineAdmin(admin.TabularInline):
     classes = ['collapse']
 
 
+class ProductInlineAdmin(admin.TabularInline):
+    model = Product
+    extra = 0
+    fields = 'model', 'category',
+    show_change_link = True
+    classes = ['collapse']
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = 'id', 'model', 'category', 'alternate_models',
@@ -104,6 +112,7 @@ class SpiderResultAdmin(admin.ModelAdmin):
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
     list_display = 'name', 'image', 'website',
+    inlines = ProductInlineAdmin,
 
 
 def get_admin_urls(urls):
