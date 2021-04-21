e79cc8c4e38fc2041c3f5e40f186741cf3cd3b83
Mark O'Reilly <marko@megsupporttools.com>
2021-04-21 08:48:50 +0100

    added dashboard admin

--- cms/dashboard/admin.py
+++ cms/dashboard/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from cms.dashboard.models import CategoryTable, CategoryGapAnalysisReport, CategoryTableAttribute
+
+
+class CategoryTableAttributeInlineAdmin(admin.TabularInline):
+    model = CategoryTableAttribute
+    extra = 0
+    fields = 'attribute',
+    show_change_link = True
+
+
+@admin.register(CategoryTable)
+class CategoryTableAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name', 'user', 'category',
+    inlines = CategoryTableAttributeInlineAdmin,
+
+
+@admin.register(CategoryGapAnalysisReport)
+class CategoryGapAnalysisReportAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name', 'user', 'category', 'brand',
