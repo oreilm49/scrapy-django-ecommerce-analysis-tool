c68f8f744283f04cc9fb8ab3c71814864df07a16
Mark O'Reilly <marko@megsupporttools.com>
2021-04-22 08:37:03 +0100

    create brand, and add to report

--- cms/dashboard/scripts/load_dashboard.py
+++ cms/dashboard/scripts/load_dashboard.py
@@ -2,13 +2,14 @@ from django.contrib.auth.models import User
 from django.db import transaction
 
 from cms.dashboard.models import CategoryGapAnalysisReport
-from cms.models import Category
+from cms.models import Category, Brand
 
 
 def load_dashboard():
     washers: Category = Category.objects.get(name="washing machines")
     admin: User = User.objects.get(username="admin")
-    CategoryGapAnalysisReport.objects.create(user=admin, name="test", category=washers, brand="indesit")
+    indesit, _ = Brand.objects.get_or_create(name="indesit")
+    CategoryGapAnalysisReport.objects.create(user=admin, name="test", category=washers, brand=indesit)
 
 
 @transaction.atomic()
